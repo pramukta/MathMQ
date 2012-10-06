3d161f637d7e5d8572b777c6802590b1464fad5b
Pramukta Kumar <prak@mac.com>
2012-10-06 14:44:30 -0400

    Message Collector and RabbitMQ Client Jars

--- MathMQ/MathMQ.m
+++ MathMQ/MathMQ.m
@@ -6,19 +6,38 @@ BeginPackage["MathMQ`", {"JLink`"}]
 (* Exported symbols added here with SymbolName::usage *) 
 
 RabbitConnect[exchange_String, queue_String, type_String, amqp_List] := 
-	Module[{client = JavaNew["edu.georgetown.mathmq.RabbitMQClient", amqp[[1]], amqp[[2]], amqp[[3]], amqp[[4]], amqp[[5]]]},
-		Print[client@connect[exchange, type, queue]];
+	Module[{client},
+		RabbitInit[];
+		client = JavaNew["edu.georgetown.mathmq.RabbitMQClient", amqp[[1]], amqp[[2]], amqp[[3]], amqp[[4]], amqp[[5]]];
+		client@connect[exchange, type, queue];
 		Return[client];
 	];
+
+RabbitCollect[client_, t_, callback_Function] :=
+	Module[{message, t0 = SessionTime[]},
+		RabbitInit[];
+		Return[
+			Reap[
+			While[SessionTime[] - t0 < t,
+				message = client@receive[];	
+				If[!SameQ[message, ""], Sow[{message, callback[message]}]];
+			]]
+		]
+	];
+
+RabbitClose[client_JavaObject] := client@close[];
 
 Begin["`Private`"]
 (* Implementation of the package *)
 
-$RABBITLIB = "E:\\rabbitmq-java-client";
-
-InstallJava[];
-AddToClassPath[FileNameJoin[{$RABBITLIB, "rabbitmq-client.jar"}]];
-LoadJavaClass["edu.georgetown.mathmq.RabbitMQClient"];
+RabbitInit[] := Module[{packagePath, rabbitjar},
+	InstallJava[];
+	packagePath = ParentDirectory@DirectoryName[FindFile["MathMQ`"]];
+	rabbitjar = FileNameJoin[{packagePath, "ext", "rabbitmq-client.jar"}];
+	If[!MemberQ[JavaClassPath[], rabbitjar],
+		AddToClassPath[rabbitjar]
+	]
+];
 
 End[]
 
